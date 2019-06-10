732a9933bea4c60b24719d86a55c3039a6a36420
madchad <irsyad.muhd@gmail.com>
2019-06-10 16:09:45 +0800

    Updates

---
+++ precache-manifest.df13c0e01bbad507102780184e16f621.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91c71e3d1eae077ca2f0c192560806f3",
+    "url": "/splyt-app/index.html"
+  },
+  {
+    "revision": "1c139962ca855824ea23",
+    "url": "/splyt-app/static/css/2.20d67eb1.chunk.css"
+  },
+  {
+    "revision": "8f7e0994fe82ce78e8b1",
+    "url": "/splyt-app/static/css/main.09b069da.chunk.css"
+  },
+  {
+    "revision": "1c139962ca855824ea23",
+    "url": "/splyt-app/static/js/2.870a8999.chunk.js"
+  },
+  {
+    "revision": "8f7e0994fe82ce78e8b1",
+    "url": "/splyt-app/static/js/main.d18b601f.chunk.js"
+  },
+  {
+    "revision": "97b54d7e0054f792ef17",
+    "url": "/splyt-app/static/js/runtime~main.6a458259.js"
+  }
+]);
